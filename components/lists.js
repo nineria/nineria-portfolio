f335abb7c4b3d3fc58858a724c12327295daae01
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-09-08 14:37:04 +0700

    feat: New style

--- components/lists.js
+++ components/lists.js
@@ -1,10 +1,22 @@
+import { Box } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import NextLink from 'next/link';
 
-const Lists = styled.li`
+export const HeadingStyle = styled.p`
+  text-align: justify;
+`;
+
+export const Member = styled.li`
   text-align: justify;
   text-indent: 1em;
 `;
+
+export const Group = ({ title, children }) => (
+  <Box mb={2}>
+    <HeadingStyle>{title}</HeadingStyle>
+    {children}
+  </Box>
+);
 
 // const Lists = ({ href = '/', children }) => {
 //   return (
@@ -17,5 +29,3 @@ const Lists = styled.li`
 //     </StyledList>
 //   );
 // };
-
-export default Lists;
