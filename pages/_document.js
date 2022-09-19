1b68ff70cfb4fbb7911204259c4b88c1ab9e4794
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-09-19 11:46:16 +0700

    feat: change thumbnail image
    - work

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
 import theme from '../lib/theme';
 
 export default class Document extends NextDocument {
